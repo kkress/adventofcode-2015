ad413a7b449f9147e3b990583bf4abf721a3ed8d
Kevin Kress <kkress@gmail.com>
2016-05-24 09:49:16 -0700

    working part 1 and 2 of 16

--- 16/src/main.rs
+++ 16/src/main.rs
@@ -3,31 +3,48 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::collections::HashMap;
 
-fn is_fuzzy_match(all: &HashMap<&str, i32>, partial: &HashMap<&str, i32>) -> bool {
-   for (k, v) in partial {
-      if !match all.get(k) {
-         Some(val) => val == v,
-         None => false
-      } { return false }
-   }
-   return true
+fn is_fuzzy_match(target: &HashMap<&str, i32>, candidate: &HashMap<&str, i32>) -> bool {
+    for (k, v) in candidate {
+        if !match target.get(k) {
+            Some(val) => val == v,
+            None => false,
+        } {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn is_very_fuzzy_match(target: &HashMap<&str, i32>, candidate: &HashMap<&str, i32>) -> bool {
+    for (k, v) in candidate {
+        let target_val = target.get(k).unwrap();
+        let is_match = match *k {
+            "cats" | "trees" => v > target_val,
+            "pomeranians" | "goldfish" => v < target_val,
+            _ => v == target_val,
+        };
+        if !is_match {
+            return false;
+        }
+    }
+    return true;
 }
 
 fn main() {
     let f = File::open("input.txt").unwrap();
     let line_buffer = BufReader::new(&f);
-   
-    let mut search = HashMap::new();
-    search.insert("children", 3);
-    search.insert("cats", 7);
-    search.insert("samoyeds", 2);
-    search.insert("pomeranians", 3);
-    search.insert("akitas", 0);
-    search.insert("vizslas", 0);
-    search.insert("goldfish", 5);
-    search.insert("trees", 3);
-    search.insert("cars", 2);
-    search.insert("perfumes", 1);
+
+    let mut target = HashMap::new();
+    target.insert("children", 3);
+    target.insert("cats", 7);
+    target.insert("samoyeds", 2);
+    target.insert("pomeranians", 3);
+    target.insert("akitas", 0);
+    target.insert("vizslas", 0);
+    target.insert("goldfish", 5);
+    target.insert("trees", 3);
+    target.insert("cars", 2);
+    target.insert("perfumes", 1);
 
     for line in line_buffer.lines() {
         let curr = line.unwrap();
@@ -36,16 +53,19 @@ fn main() {
         let number = name_parts[0].split(' ').collect::<Vec<_>>()[1].parse::<i32>().unwrap();
         let parts = name_parts[1]
             .split(',')
-            .map(|x| {
-               x.trim().split_whitespace().map(|x| x.trim_matches(':')).collect::<Vec<_>>()
-            }).collect::<Vec<_>>();
+            .map(|x| x.trim().split_whitespace().map(|x| x.trim_matches(':')).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
         let mut attr = HashMap::new();
         for p in parts {
-          attr.insert(p[0], p[1].parse::<i32>().unwrap());
+            attr.insert(p[0], p[1].parse::<i32>().unwrap());
         }
 
-        if is_fuzzy_match(&search, &attr) {
-          println!("Match Sue #{} {:?}", number, attr); 
+        if is_fuzzy_match(&target, &attr) {
+            println!("Part1 Match Sue #{} {:?}", number, attr);
+        }
+
+        if is_very_fuzzy_match(&target, &attr) {
+            println!("Part2 Match Sue #{} {:?}", number, attr);
         }
     }
 }
